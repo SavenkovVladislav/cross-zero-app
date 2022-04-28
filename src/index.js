78004a09ec38c59592f923c4a5ab4fa9085fca1a
Vlad <savenkov.vla@yandex.ru>
2022-04-28 18:17:27 +0700

    code before the announcement of the winner

--- src/index.js
+++ src/index.js
@@ -3,35 +3,54 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.setState({ value: 'X' })}
-            >
-                {this.state.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button
+            className='square'
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    );
 }
 
 
-
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            squares: Array(9).fill(null), // метод fill() заполняет все элементы массива от начального до конечного индексов одним значением. 
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice(); // метод slice() возвращает новый массив, содержащий копию части или всего исходного массива.
+        squares[i] = this.state.xIsNext ? 'X' : 'O'; // в зависимости от того this.state.xIsNext = true или false мы записываем в i-тый элемент массива squares 'X' или 'O'
+
+        this.setState({
+            squares: squares, // здесь мы присваеваем ключу squares значение переменной squares
+            xIsNext: !this.state.xIsNext, // здесь мы инвертируем значение ключа xIsNext
+        });
+
+        console.log('переменная', squares);
+        console.log('массив', this.state.squares); // почему запись "X" в this.state.squares происходит с задержкой в итерацию?
+
+
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)} //?
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Следующий игрок: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
