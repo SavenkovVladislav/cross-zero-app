73196cc13a3451a6da844746447f240c707ac2ad
Vlad <savenkov.vla@yandex.ru>
2022-05-10 19:59:37 +0700

    announcement of the winner

--- src/index.js
+++ src/index.js
@@ -27,30 +27,41 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice(); // метод slice() возвращает новый массив, содержащий копию части или всего исходного массива.
+        // осбсудить этот if
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O'; // в зависимости от того this.state.xIsNext = true или false мы записываем в i-тый элемент массива squares 'X' или 'O'
 
         this.setState({
             squares: squares, // здесь мы присваеваем ключу squares значение переменной squares
             xIsNext: !this.state.xIsNext, // здесь мы инвертируем значение ключа xIsNext
-        });
+        },
+            () => {
+                console.log('массив', this.state.squares); // здесь мы вызываем стрелочную функицю, которая называется callback, это второй аргумент функции setState, которая вызывается после того, как setState закончил свою работу
+            });
 
         console.log('переменная', squares);
-        console.log('массив', this.state.squares); // почему запись "X" в this.state.squares происходит с задержкой в итерацию?
-
-
     }
 
     renderSquare(i) {
         return (
             <Square
                 value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)} //?
+                onClick={() => this.handleClick(i)}
             />
         );
     }
 
     render() {
-        const status = 'Следующий игрок: ' + (this.state.xIsNext ? 'X' : 'O');
+
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Победитель: ' + winner;
+        } else {
+            status = 'Следующий игрок: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -89,6 +100,28 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+// фунция определения победителя
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]; // const [a, b, c] это специальная запись создания нескольких перемынных сразу или деструктуризация, в нашем случае мы записываем в переменные a,b,c, которые мы создаем, i-тый элемент массива lines, т.е. a,b,c при первой итерации цикла будут равны 0,1,2
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 // ========================================
