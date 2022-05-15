44629e8d754dd22210d0e3a82511c37dd4063121
Vlad <savenkov.vla@yandex.ru>
2022-05-15 15:43:29 +0700

    adding time travel

--- src/index.js
+++ src/index.js
@@ -16,56 +16,18 @@ function Square(props) {
 
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            squares: Array(9).fill(null), // метод fill() заполняет все элементы массива от начального до конечного индексов одним значением. 
-            xIsNext: true,
-        };
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice(); // метод slice() возвращает новый массив, содержащий копию части или всего исходного массива.
-        // осбсудить этот if
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O'; // в зависимости от того this.state.xIsNext = true или false мы записываем в i-тый элемент массива squares 'X' или 'O'
-
-        this.setState({
-            squares: squares, // здесь мы присваеваем ключу squares значение переменной squares
-            xIsNext: !this.state.xIsNext, // здесь мы инвертируем значение ключа xIsNext
-        },
-            () => {
-                console.log('массив', this.state.squares); // здесь мы вызываем стрелочную функицю, которая называется callback, это второй аргумент функции setState, которая вызывается после того, как setState закончил свою работу
-            });
-
-        console.log('переменная', squares);
-    }
-
     renderSquare(i) {
         return (
             <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]} // в компоненте Boart мной не описан конструктор с объявленными пропасами, но по умолчанию, он существует и принимает их.
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
 
     render() {
-
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Победитель: ' + winner;
-        } else {
-            status = 'Следующий игрок: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -87,15 +49,84 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [ // здесь создается массив, элементом которого является объект. Зачем массив? Затем, чтобы в него можно было записывать историю ходов, которая будет храниться в объектах.
+                {
+                    squares: Array(9).fill(null),
+                },
+            ],
+            stepNumber: 0,
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O'; // при первом нажатии на правый нижний угол доски масив squares будет выглядить следующим образом [null, null, null, null, null, null, null, null, 'X']
+
+        this.setState({ // пример: [1, 2, 3].concat([4, 5, 6]) -> результатом этого будет массив [1, 2, 3, 4, 5, 6]
+            history: history.concat([
+                {
+                    squares: squares
+                }
+            ]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,
+        }, () => console.log(this.state.history));
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+
     render() {
+        const history = this.state.history;
+        //const current = history[history.length - 1]; // запись history[...] означает, что мы обращаемся к элементу массива history под каким-то номером, в нашем случае обращение идет именно к последнему элементу и записываем этот элемент массива в переменную current.
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares); // вызывается функция calculateWinner, в параметрах вызывается свойство squares объекта current, который описан строчкой выше
+        // ?
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        })
+
+        let status;
+        if (winner) {
+            status = 'Победитель: ' + winner;
+        } else {
+            status = 'Следующий игрок: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
